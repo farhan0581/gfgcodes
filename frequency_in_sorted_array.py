257c807f56e614eca14ac56af92b8c19a0c224d5
farhan0581 <farhan0581@gmail.com>
2018-03-20 09:52:54 +0530

    more changes
    more changes

--- frequency_in_sorted_array.py
+++ frequency_in_sorted_array.py
@@ -1,8 +1,21 @@
 class FindFreq(object):
 	"""docstring for FindFreq"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self, *args):
+		self.arr = args[0]
+		self.elem = args[1]
 
-	def binary_search(self, arr):
+	def first_occurance(self, start, end):
+		mid = (start + end) / 2
+		
+		if self.arr[mid] == self.elem and (mid==0 or self.arr[mid-1] < self.elem):
+			return mid
+		
+		elif self.arr[mid] < self.elem:
+			return self.first_occurance()
+
+
+
+
+	def last_occurance(self, arr, start, end, elem):
 		pass
 		
